3b2675099eff0ce268b3b8ccbe186e66281c81f4
McKayla Lankau <lankaukk@yahoo.com>
2022-02-22 22:04:03 -0500

    fixed deploy error again

--- src/components/PicList.js
+++ src/components/PicList.js
@@ -1,15 +1,6 @@
 import React from 'react'
 
 const PicList = (props) => {
-  // console.log(props);
-
-  function changeSize(e) {
-    e.target.style.transform = "scale(2)";
-  }
-
-  // function removeSize(e) {
-  //   e.target.style.transform = "scale(3)";
-  // }
 
   if (props.pics.length > 0) {
     return (
@@ -17,8 +8,6 @@ const PicList = (props) => {
           {props.pics.map(pic =>     
             <>
                 <div 
-                // onMouseEnter={changeSize}
-                // onMouseLeave={removeSize}
                 className='pexel'
                 style={{height: '16.6%', width: '16.6%', backgroundColor: pic.avg_color }}></div>
             </>
