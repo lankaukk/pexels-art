b14b484dbb26ac7ba241981b58e1a880b09bb6b2
McKayla Lankau <lankaukk@yahoo.com>
2022-02-22 21:59:10 -0500

    added favicon

--- src/components/PicList.js
+++ src/components/PicList.js
@@ -2,12 +2,25 @@ import React from 'react'
 
 const PicList = (props) => {
   // console.log(props);
+
+  function changeSize(e) {
+    e.target.style.transform = "scale(2)";
+  }
+
+  // function removeSize(e) {
+  //   e.target.style.transform = "scale(3)";
+  // }
+
   if (props.pics.length > 0) {
     return (
       <div className="picCard">
           {props.pics.map(pic =>     
             <>
-                <div style={{height: '16.6%', width: '16.6%', backgroundColor: pic.avg_color }}></div>
+                <div 
+                onMouseEnter={changeSize}
+                onMouseLeave={removeSize}
+                className='pexel'
+                style={{height: '16.6%', width: '16.6%', backgroundColor: pic.avg_color }}></div>
             </>
           )}
       </div>
